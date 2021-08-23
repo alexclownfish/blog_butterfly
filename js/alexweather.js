b4cbbce0222b1a5703e630bf18c7ea9daa1c70a8
alexclownfish <ywz0207@163.com>
2021-08-23 12:35:55 +0800

    “提交说明”

---
+++ js/alexweather.js
@@ -0,0 +1,23 @@
+WIDGET = {
+    CONFIG: {
+        "modules": "0123",
+        "background": 5,
+        "tmpColor": "ffffff",
+        "tmpSize": 16,
+        "cityColor": "ffffff",
+        "citySize": 16,
+        "aqiSize": 16,
+        "weatherIconSize": 24,
+        "alertIconSize": 18,
+        "padding": "10px 10px 10px 10px",
+        "shadow": "0",
+        "language": "zh",
+        "borderRadius": 5,
+        "fixed": "true",
+        "vertical": "middle",
+        "horizontal": "center",
+        "left": "210",
+        "top": 10,
+        "key": "zjqNH7ovMn"
+    }
+}
